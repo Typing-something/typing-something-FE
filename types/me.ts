63c9d544fa899817d77ab7d15d4bd45dd2688410
MinSung Kwon <csstumings804@gmail.com>
2026-01-09 02:34:53 +0900

    feat: 리더보드 마이랭크 연동 및 Me 타입 통합

---
+++ types/me.ts
@@ -0,0 +1,10 @@
+export type Me = {
+    name: string;
+    handle: string;
+    tier: string;
+    email?: string;
+    image: string | null;
+    avg_accuracy: number | null;
+    max_combo: number | null;
+    play_count: number | null;
+  };
