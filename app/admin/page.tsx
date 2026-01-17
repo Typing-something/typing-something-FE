1aca5223b9ca85f0834f53d075ee2ebf8496c6b5
MinSung Kwon <csstumings804@gmail.com>
2026-01-17 22:41:43 +0900

    chore: 관리자 페이지, 마이페이지  상단 여백(pt) 위치 미세 조정

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function AdminPage(){
     return (
         <main className="min-h-screen bg-neutral-100">
-            <div className="mx-auto w-full max-w-6xl px-4 py-10 mt-14">
+            <div className="mx-auto w-full max-w-6xl px-4 py-10 pt-24">
                 관리자용 페이지
             </div>
         </main>
