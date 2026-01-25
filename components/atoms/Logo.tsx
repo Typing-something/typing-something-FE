d11e3c2efe245d3ac0020d27050d1d23aa1d114f
MinSung Kwon <csstumings804@gmail.com>
2026-01-26 01:58:33 +0900

    chore: 로고 미세 수정

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -4,12 +4,11 @@ export function Logo() {
     return (
       <Link href="/">
         <div className="flex items-center gap-2">
-          <div className="h-6 w-6 rounded-md bg-gradient-to-tr from-[#fb4058] to-[#fb576e]" />  
             <div
               className="text-lg font-semibold tracking-wide"
             >
-            LyricType
-            <span className="ml-0.5 font-bold text-lg text-[#fb4058] typing-cursor">
+            TypeSomething
+            <span className="font-extrabold text-xl text-[#fb4058] typing-cursor">
               |
             </span>
           </div>
