4bb7bda0b9cc1ef8d3be22dc6b2b8af71401904f
MinSung Kwon <csstumings804@gmail.com>
2026-01-29 23:26:29 +0900

    feat: 다음 곡 프리페치 로딩 토스트 추가

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,8 @@
 import { SessionProvider } from "next-auth/react";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Providers({
   children,
@@ -15,6 +17,18 @@ export default function Providers({
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         {children}
+        <ToastContainer
+          position="bottom-right"
+          autoClose={false}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick={false}
+          closeButton={false}
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          theme="light"
+        />
       </QueryClientProvider>
     </SessionProvider>
   )
